fd70ed174f28259982ec1f1f85158347a6eb9b18
dfd <dfd@justdfd.com>
2018-10-14 08:53:55 -0400

    Initial Commit

---
+++ UINavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigationController
+//
+//  Created by Dave Dombrowski on 10/14/18.
+//  Copyright © 2018 justDFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
